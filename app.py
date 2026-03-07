317d234a3c9c87f8a971a6366d55a4c8e7baa357
Sirawdink Abayneh <sirawdinkabayneh77@gmail.com>
2026-03-07 21:53:21 +0300

    Testing API

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+import requests
+from config import NEWS_API_KEY
+
+app = Flask(__name__)
+
+# Homepage - Route
+@app.route("/")
+def index():
+   query = request.args.get("query", "Latest")
+   url = f"https://newsapi.org/v2/everything?q={query}&apiKey={NEWS_API_KEY}"
+   response = requests.get(url)
+   news_data = response.json()
+   print(news_data)
+
+   return "Testing"
+
+
+
+
+
+
+
+if __name__ == "__main__":
+   app.run(debug=True)
